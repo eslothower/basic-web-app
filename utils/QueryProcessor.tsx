76f4647b6f9473e817ea78825a533d750d90b874
Eli Slothower <eslothow@andrew.cmu.edu>
2023-03-14 16:35:25 -0400

    Update QueryProcessor.tsx

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -7,7 +7,13 @@ export default function QueryProcessor(query: string): string {
       return (String(Number(match[0]) + Number(match[1])));
     }
     
-  } 
+  } else if (query.match(/^What is ([0-9]*) minus ([0-9]*)\?$/)) {
+    var match = query.match(/\d+/g);
+    if (match != null){
+      return (String(Number(match[0]) - Number(match[1])));
+    }
+    
+  }
   //  else if (query.includes("Which of the following numbers is the largest: \d+, \d+, \d+?")){
   //   var match = query.match(/\d+/g);    
   //   return (String(Math.max(Number(match[0]), Number(match[1]), Number(match[2]))));
