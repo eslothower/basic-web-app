b74b3e6c35d50899b7eb423dc3c78e82016348dd
Eli Slothower <eslothow@andrew.cmu.edu>
2023-03-14 16:21:01 -0400

    Update QueryProcessor.tsx

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,11 +1,12 @@
 export default function QueryProcessor(query: string): string {
-  if (query.includes("What is")) {
-    var match = query.match( /^What is \d+ plus \d+\?$/);
-    return (String(match[0] + match[1]));
-  } else if (query.includes("Which of the following numbers is the largest: \d+, \d+, \d+?")){
-    var match = query.match(/Which of the following numbers is the largest: ([0-9]*), ([0-9]*), ([0-9]*)?/);
-    return (String(Math.max(Number(match[1]), Number(match[2]), Number(match[3]))));
-  }
 
+  // if (query.includes("What is")) {
+  //   var match = query.match( /^What is \d+ plus \d+\?$/);
+  //   return (String(match[0] + match[1]));
+  // } else if (query.includes("Which of the following numbers is the largest: \d+, \d+, \d+?")){
+  //   var match = query.match(/Which of the following numbers is the largest: ([0-9]*), ([0-9]*), ([0-9]*)?/);
+  //   return (String(Math.max(Number(match[1]), Number(match[2]), Number(match[3]))));
+  // }
+  console.log("THIS IS WORKING");
   return "no";
 }
