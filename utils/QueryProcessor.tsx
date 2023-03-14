24a0eeaf8ce405a041c89006415a75fc2ae2300b
Eli Slothower <eslothow@andrew.cmu.edu>
2023-03-14 16:25:42 -0400

    Update QueryProcessor.tsx

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,12 +1,13 @@
 export default function QueryProcessor(query: string): string {
-  console.log("THIS IS ;alskdfj;lkja");
+  console.log("THIS IS CHICKEN");
 
-  // if (query.includes("What is")) {
-  //   var match = query.match(/\d+/g);
-  //   return (String(match[0] + match[1]));
-  // } else if (query.includes("Which of the following numbers is the largest: \d+, \d+, \d+?")){
+  if (query.includes("What is")) {
+    var match = query.match(/\d+/g);
+    return (String(match[0] + match[1]));
+  } 
+  //  else if (query.includes("Which of the following numbers is the largest: \d+, \d+, \d+?")){
   //   var match = query.match(/\d+/g);    
   //   return (String(Math.max(Number(match[0]), Number(match[1]), Number(match[2]))));
   // }
-  return ";laskdjf";
+  return "moose";
 }
