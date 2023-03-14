2f10e885dd80ef502758096e46b1d84700f6198a
Eli Slothower <eslothow@andrew.cmu.edu>
2023-03-14 15:50:56 -0400

    Update QueryProcessor.tsx

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,11 +1,9 @@
 export default function QueryProcessor(query: string): string {
-  if (query.toLowerCase().includes("shakespeare")) {
+  if (query.includes("What is your name?")) {
     return (
-      "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-      "English poet, playwright, and actor, widely regarded as the greatest " +
-      "writer in the English language and the world's pre-eminent dramatist."
+      "Eli"
     );
   }
 
-  return "";
+  return "error";
 }
