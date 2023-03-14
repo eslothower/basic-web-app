30be3fd4c818c25355e448c60f398a639d6e1b8e
Eli Slothower <eslothow@andrew.cmu.edu>
2023-03-14 16:07:06 -0400

    Update QueryProcessor.tsx

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,8 +1,11 @@
 export default function QueryProcessor(query: string): string {
-  if (query.includes("What is your name?")) {
+  if (query.includes("What is \d+ plus \d+?")) {
+    var match = query.match(/^What is ([0-9]*)*plus*([0-9]*)\?*$/);
     return (
-      "Eli and Frank"
+      match[1] + match[2];
     );
+  } else if (query.includes("Which of the following numbers is the largest: \d+, \d+, \d+?")){
+    return 1;
   }
 
   return "error";
